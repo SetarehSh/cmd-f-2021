331298b8f150f65b08c43b8d61517419d63db286
clam169 <jojilam@gmail.com>
2021-03-06 18:48:33 -0800

    finished registration

--- scripts/validator.js
+++ scripts/validator.js
@@ -7,7 +7,7 @@
 //   })
 // })
 const form = document.getElementById('registration');
-
+const checkbox = document.getElementById('agreeCheckbox');
 function formValidation() {
 
 }
@@ -15,23 +15,36 @@ let password = document.getElementById('password');
 let firstName = document.getElementById('firstName');
 let lastName = document.getElementById('lastName');
 let confirmPassword = document.getElementById('confirmPassword')
- 
+let language = document.getElementById('language')
 //event listener
+checkbox.addEventListener('click', toggleButton)
+
+
+
+function toggleButton() {
+  let submitBtn = document.getElementById('submitBtn')
+  if(submitBtn.style.display === "block") {
+    submitBtn.style.display = "none"
+  } else {
+    submitBtn.style.display = "block"
+  }
+}
 form.addEventListener('submit', submit);
 
 //forms
 function submit(event) {
   event.preventDefault();
-  let check = false;
-  if (checkName(firstName)) {
-    if (checkName(lastName)) {
-      if (validatePassword(password)) {
-        location.replace("./dashboard.html");
+  if (checkLength(firstName)) {
+    if (checkLength(lastName)) {
+      if (checkLength (language)) {
+        if (validatePassword(password)) {
+          location.replace("./dashboard.html");
+          break;
+        }
       }
     }
   }
-  console.log("did not pass check")
-
+  alert('Please check your input.')
 }
 
 // function checkPW (password) {
@@ -48,13 +61,14 @@ function submit(event) {
 //   }
 // }
 
-function checkName (name) {
-  console.log(name.value)
-  console.log(name.value.length)
-  return name.value.length > 2;
-
+function checkLength (input) {
+  if (input.value.length >= 2) {
+    return true;
+  } else {
+    alert('Please enter two or more characters.')
+    return false;
+  }
 }
-
 
 function  hasNumber(str){
   return str.split("").some((ch) => parseInt(ch))
@@ -63,23 +77,21 @@ function  hasNumber(str){
 function hasSymbol (str){
   const characters = ["!", "@", "#", "?"];
   for (const character of characters){
-      if (str.includes(character)){
-          return true;
-      }
+    if (str.includes(character)){
+      return true;
+    }
   }
   return false;
 }
 
 function validatePassword(password) {
-    
   if (password.value.length >= 8){
-      if(hasNumber(password.value)){
-          if(hasSymbol(password.value)){
-              return true;
-          }
+    if(hasNumber(password.value)){
+      if(hasSymbol(password.value)){
+        return true;
       }
+    }
   }
+  alert('Your password is invalid. Please set a password contain at least 8 characters, one special letter and one number')
   return false;
 }
-
-
